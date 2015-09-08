a8f5d17a13f27a943bfda609968e0b11bfee2338
Krishna <softeng.krishna@gmail.com>
2015-09-09 00:25:06 +0530

    cscscs

--- app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -426,7 +426,7 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f020052;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020053;
 		public static final int abc_textfield_search_material = 0x7f020054;
-		public static final int notification_template_icon_bg = 0x7f020084;
+		public static final int notification_template_icon_bg = 0x7f020087;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f0d0089;
